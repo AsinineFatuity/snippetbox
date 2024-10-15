71faa7c9f2246f129a5800f8cb70feb15b4a74b4
Kembo Franklyne <asininefatuity@gmail.com>
2024-10-15 17:39:28 +0300

    refactored code using map type

--- main.go
+++ main.go
@@ -5,14 +5,27 @@ import (
 	"net/http"
 )
 
+const homeURL string = "/"
+const showSnippetURL string = "/snippet"
+const createSnippetURL string = "/snippet/create"
+
 func home(w http.ResponseWriter, r *http.Request) {
-	returnText := []byte("Hello from snippet box")
-	w.Write(returnText)
+	w.Write([]byte("Home page..."))
+}
+
+func showSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific snippet..."))
+}
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create a new snippet..."))
 }
 
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
+	urlHandlerMap := map[string]http.HandlerFunc{homeURL: home, showSnippetURL: showSnippet, createSnippetURL: createSnippet}
+	for url, handler := range urlHandlerMap {
+		mux.HandleFunc(url, handler)
+	}
 	log.Println("Starting server on port 4000")
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
