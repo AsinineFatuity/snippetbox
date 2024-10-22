f685e7af15d090ee110c8c60f01967c2420cb576
Kembo Franklyne <asininefatuity@gmail.com>
2024-10-22 22:29:20 +0300

    adjusted error stack frame

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -10,7 +10,7 @@ import (
 func (app *application) serverError(w http.ResponseWriter, err error) {
 	// get the stack trace for the current go routine and append to the log message
 	trace := fmt.Sprintf("%s\n%s", err.Error(), debug.Stack())
-	app.errorLog.Println(trace)
+	app.errorLog.Output(2, trace)
 	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 }
 
