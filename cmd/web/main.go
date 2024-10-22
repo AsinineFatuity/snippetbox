9880ce2821a81f7eee5ccf76ea5a18e50881e493
Kembo Franklyne <asininefatuity@gmail.com>
2024-10-22 21:29:13 +0300

    used same log for server

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -28,7 +28,12 @@ func main() {
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle(staticURL, http.StripPrefix("/static", fileServer))
 
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  mux,
+	}
 	infoLog.Printf("Starting server on port %s", *addr)
-	err := http.ListenAndServe(*addr, mux)
+	err := srv.ListenAndServe()
 	errorLog.Fatal(err)
 }
